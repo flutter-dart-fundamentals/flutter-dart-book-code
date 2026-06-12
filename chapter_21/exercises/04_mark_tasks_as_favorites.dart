0716689b1fa504489fbf51d32eba90589c0183e8
Shereef Abu Almaati <ashereef@example.com>
2026-06-12 19:55:22 +0300

    Rename favourites exercise to US spelling

--- chapter_21/exercises/04_mark_tasks_as_favorites.dart
+++ chapter_21/exercises/04_mark_tasks_as_favorites.dart
@@ -1,13 +1,13 @@
-// Exercise 04 - Mark Tasks as Favourites
+// Exercise 04 - Mark Tasks as Favorites
 
 /* Add a star icon to the leading slot that the user can tap to mark a task as
-a favourite. Tapping the star toggles between an outlined star
+a favorite. Tapping the star toggles between an outlined star
 (Icons.star_border) and a filled yellow star (Icons.star). Unlike the delete
 action, this one is reversible, so you can let the user tap the star
 directly without a confirmation dialog. This exercise reinforces the UX
 principle from this chapter: reversible actions can have small, direct
 targets because the worst case is just a quick re-tap. To track which tasks
-are favourites you will need a second list, perhaps a List<bool> running
+are favorites you will need a second list, perhaps a List<bool> running
 parallel to _todos. */
 
 
